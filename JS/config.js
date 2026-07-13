e258bfb906d63d9052c5b4470fa4d4992cb6c3b2
Spacklight <prof200619@gmail.com>
2026-07-13 15:07:32 +0200

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,1 +1,8 @@
-
+// GitHub repo configuration
+const CONFIG = {
+  GITHUB_USERNAME: "Spacklight",   // <-- change this
+  DATA_REPO: "music-share-data",             // repo holding tracks.json + uploads/
+  BRANCH: "main",
+  TRACKS_FILE: "tracks.json",
+  UPLOADS_FOLDER: "uploads"
+};
